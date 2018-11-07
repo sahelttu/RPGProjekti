daac46fe63fd2500fa4b8442efaaea908c98af73
Samuli Helttunen <samuli.helttunen@gmail.com>
2018-11-07 19:43:49 +0200

    Player prefabs, started to work on the Job system

---
+++ Assets/Scripts/Jobs/JobBase.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobBase {
+
+    private string JobName;
+    private string JobDescription;
+
+    private int HealthMultip;
+    private int ManaMultip;
+    private int StrengthMultip;
+    private int VitalityMultip;
+    private int AgilityMultip;
+
+    public string jobName
+    {
+        get { return JobName; }
+        set { JobName = value; }
+    }
+
+    public string jobDescription
+    {
+        get { return JobDescription; }
+        set { JobDescription = value; }
+    }
+
+    public int healthMulti
+    {
+        get { return HealthMultip; }
+        set { HealthMultip = value; }
+    }
+
+    public int manaMulti
+    {
+        get { return ManaMultip; }
+        set { ManaMultip = value; }
+    }
+
+    public int strengthMulti
+    {
+        get { return StrengthMultip; }
+        set { StrengthMultip = value; }
+    }
+
+    public int vitalityMulti
+    {
+        get { return VitalityMultip; }
+        set { VitalityMultip = value; }
+    }
+
+    public int agilityMulti
+    {
+        get { return AgilityMultip; }
+        set { AgilityMultip = value; }
+    }
+ 
+}
